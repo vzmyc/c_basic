0659969093095e3853d10b06bf2b9161d6368140
young-chan.moon <nicemyc@naver.com>
2021-09-24 17:45:37 +0900

    Go Lang Pointer receiver
    Signed-off-by: young-chan.moon <nicemyc@naver.com>

---
+++ go/pointer_receiver.go
@@ -0,0 +1,18 @@
+package main
+
+//Rect - struct 정의
+type Rect struct {
+    width, height int
+}
+
+// 포인터 Receiver
+func (r *Rect) area2() int {
+    r.width++
+    return r.width * r.height
+}
+
+func main() {
+    rect := Rect{10, 20}
+    area := rect.area2() //메서드 호출
+    println(rect.width, area) // 11 220 출력
+}
